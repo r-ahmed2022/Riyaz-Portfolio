9d8d70ba91f50bb6486a25b83080b797a4e0306e
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-06-14 05:11:29 +0530

    added mobile menu

---
+++ menu.js
@@ -0,0 +1,12 @@
+function showMenu() {     
+       document.getElementById("nav-mobile").style.width = "100%";
+      }
+
+      
+      function closebtn() {
+        document.getElementById("nav-mobile").style.width = "0%";
+      }
+    
+    function hide() {
+        document.getElementById("nav-mobile").style.display = "none";
+         }
