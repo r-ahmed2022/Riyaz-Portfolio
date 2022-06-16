a35d5bbf6afc474a940f854f156bb98e62016254
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-06-16 18:57:11 +0530

    added email validation in contact-form

--- menu.js
+++ menu.js
@@ -29,3 +29,33 @@ contactElement.addEventListener('click', () => {
   document.getElementById('nav-mobile').style.display = 'none';
   reloadBrowser();
 });
+
+function checkLowerCase(str) {
+  const validChar = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const errStr = {};
+  if (!str.match(validChar)) {
+    errStr.msg = 'Invalid email address';
+    errStr.valid = false;
+  } else if (str === String(str).toLowerCase()) {
+    errStr.valid = true;
+  } else {
+    errStr.msg = 'Email should be only in Lower case';
+  }
+  return errStr;
+}
+
+const emailValidator = document.getElementById('btn-5');
+emailValidator.addEventListener('click', (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  const emailValue = document.getElementById('email-field');
+  const Obj = checkLowerCase(emailValue.value);
+  if (Obj.valid !== true) {
+    document.getElementById('errorMsg').innerText = Obj.msg;
+    emailValue.focus();
+    emailValue.style.border = 'thick solid #ff0000';
+  } else {
+    emailValue.style.border = 'thick solid #00ff00';
+    document.getElementById('form-1').submit();
+  }
+});
