53ac09ad2c12bdf6d1cccf8974d11d428467baa1
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-06-21 18:48:45 +0530

    added dynamic projects div

---
+++ projects.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+const projects = [
+  {
+    name: 'Multi-Post Stories',
+    description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
+    languages: ['html', 'bootstrap', 'Ruby on rails'],
+    image: './images/desktop.svg',
+    liveVersion: 'See Live',
+    sourceLink: 'See Source',
+  },
+
+];
