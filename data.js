c93fb41b1fff14b37d7e8584b4e7e60e559f0841
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-06-22 15:37:40 +0530

    fixed linters

--- data.js
+++ data.js
@@ -1,47 +1,65 @@
-const card1 = document.getElementById('card-1');
-const div1left = document.createElement('div');
-div1left.setAttribute('id', 'div-1--left');
-const img = document.createElement('img');
-img.setAttribute('id', 'card-1-img');
-img.setAttribute('src', 'images/recenttitle.svg');
-img.setAttribute('alt', 'Gymfit logo');
-div1left.append(img);
-card1.append(div1left);
-const div1right = document.createElement('div');
-div1right.setAttribute('id', 'div-1--right');
-const h2 = document.createElement('h2');
-h2.setAttribute('class', 'works-title-text');
-h2.setAttribute('id', 'works-title-text');
-h2.innerHTML = 'Multi-Post Stories';
-div1right.append(h2);
-const p = document.createElement('p');
-p.innerHTML = "A daily selection of privately personalized reads no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.";
-p.setAttribute('id', 'card-1-text');
-div1right.append(p);
-const skilltext = document.createElement('div');
-skilltext.setAttribute('id', 'skill-text');
-div1right.append(skilltext);
-const ul = document.createElement('ul');
-const li1 = document.createElement('li');
-li1.setAttribute('class', 'read-text');
-li1.innerHTML = 'css';
-ul.append(li1);
-const li2 = document.createElement('li');
-li2.setAttribute('class', 'read-text');
-li2.innerHTML = 'html';
-ul.append(li2);
-const li3 = document.createElement('li');
-li3.setAttribute('class', 'read-text');
-li3.innerHTML = 'bootstrap';
-ul.append(li3);
-const li4 = document.createElement('li');
-li4.setAttribute('class', 'read-text');
-li4.innerHTML = 'Ruby';
-ul.append(li4);
-skilltext.append(ul);
-div1right.append(skilltext);
-const button = document.createElement('button');
-button.setAttribute('class', 'div-1--right-btn');
-button.innerHTML = 'See Project';
-div1right.append(button);
-card1.append(div1right);
+const projects = [
+  {
+    name: 'Multi-Post Stories',
+    description:
+          "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text",
+    languages: ['html', 'bootstrap', 'Ruby on rails'],
+    image: 'images/recenttitle.svg',
+    liveVersion: 'See Live',
+    sourceLink: 'See Source',
+  }];
+
+const buildCard1 = (projects) => {
+  const card1 = document.querySelector('.card-1');
+  const div1left = document.createElement('div');
+  div1left.setAttribute('id', 'div-1--left');
+  const img = document.createElement('img');
+  img.setAttribute('id', 'card-1-img');
+  img.setAttribute('src', projects.image);
+  img.setAttribute('alt', 'Gymfit logo');
+  div1left.append(img);
+  card1.append(div1left);
+  const div1right = document.createElement('div');
+  div1right.setAttribute('id', 'div-1--right');
+  card1.append(div1right);
+  const h2 = document.createElement('h2');
+  h2.setAttribute('class', 'works-title-text');
+  h2.setAttribute('id', 'works-title-text-1');
+  h2.innerHTML = projects.name;
+  div1right.append(h2);
+  const p = document.createElement('p');
+  p.setAttribute('id', 'card-1-text');
+  p.innerHTML = projects.description;
+  div1right.append(p);
+  const skilltext = document.createElement('div');
+  skilltext.setAttribute('id', 'skill-text');
+  div1right.append(skilltext);
+  const ul = document.createElement('ul');
+  for (let i = 0; i < projects.languages.length;) {
+    const li = document.createElement('li');
+    const a1 = document.createElement('a');
+    a1.setAttribute('class', 'read-text');
+    a1.innerHTML = projects.languages[i];
+    li.append(a1);
+    ul.append(li);
+    // eslint-disable-next-line no-plusplus
+    i++;
+  }
+  skilltext.append(ul);
+  div1right.append(skilltext);
+  const button = document.createElement('button');
+  button.setAttribute('class', 'div-1--right-btn');
+  button.setAttribute('id', 'div-1--right-btn');
+  button.setAttribute('data-open', 'modal1');
+  button.innerHTML = 'See Project';
+  div1right.append(button);
+};
+
+projects.forEach((projectPage) => buildCard1(projectPage));
+
+const displayProject = document.querySelectorAll('[data-open]');
+// eslint-disable-next-line no-unused-vars
+const isVisible = 'is-visible';
+displayProject.addEventListener('click', () => {
+  alert('display project');
+});
--- projects.js
+++ projects.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 // eslint-disable-next-line no-unused-vars
 const projects = [
   {
@@ -5,9 +6,8 @@ const projects = [
     description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
     languages: ['html', 'bootstrap', 'Ruby on rails'],
-    image: './images/desktop.svg',
+    image: 'images/recenttitle.svg',
     liveVersion: 'See Live',
     sourceLink: 'See Source',
-  },
-
-];
+  }];
+export default projects;
