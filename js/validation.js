8712b11a421b144568831f722f7cfee1b6612e40
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-06-25 09:28:34 +0530

    fixed email validation error position

--- js/validation.js
+++ js/validation.js
@@ -1,67 +1,22 @@
-/* eslint-disable no-unused-vars */
 const form = document.getElementById('form-1');
-const fname = document.getElementById('fname');
-const lname = document.getElementById('lname');
 const email = document.getElementById('email-field');
-const comments = document.getElementById('comments');
-
-const setError = (element, message) => {
-  const inputControl = element.parentElement.nextElementSibling;
-  const errorDisplay = inputControl.querySelector('.errorMsg');
-  errorDisplay.innerText = message;
-  inputControl.classList.add('error');
-  inputControl.classList.remove('success');
-};
-
-let counter = 0;
-
-const validateForm = () => {
-  const firstname = fname.value.trim();
-  const lastname = lname.value.trim();
-  const emailField = email.value.trim();
-  const commentValue = comments.value.trim();
-  if (firstname === '') {
-    setError(fname, 'first name is required');
-    return;
-  } if (firstname.split('').length > 30) {
-    setError(fname, 'first name should not exceed 30 characters');
-    return;
+const lname = document.getElementById('lname');
+const validateEmail = () => {
+  if (email.value !== email.value.toLowerCase()) {
+    const inputControl = email.parentElement.nextElementSibling;
+    inputControl.classList.add('error');
+    const displayError = document.getElementById('errorMsg');
+    displayError.innerHTML = 'Email should be only in Lower Case';
+    return false;
   }
-
-  counter += 1;
-
-  if (lastname === '') {
-    setError(lname, 'last name is required');
-    return;
-  } if (lastname.split('').length > 30) {
-    setError(lname, 'last name should not exceed 30 characters');
-    return;
-  }
-  counter += 1;
-
-  if (emailField === '') {
-    setError(email, 'Email is required');
-    return;
-  } if (emailField !== emailField.toLowerCase()) {
-    setError(email, 'Email should be in lowercase');
-    return;
-  }
-  counter += 1;
-
-  if (commentValue === '') {
-    setError(comments, 'Please add a message');
-  } else if (commentValue.split('').length > 500) {
-    setError(comments, 'Message should be short and do not exceed 500 characters');
-  }
-  counter += 1;
+  return true;
 };
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  validateForm();
-  if (counter >= 3) {
+  e.stopPropagation();
+  if (validateEmail()) {
+    lname.attributes.required = false;
     form.submit();
-  } else {
-    counter = 0;
   }
 });
